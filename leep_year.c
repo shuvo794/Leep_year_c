c7fc92ca7ab1fb8ea6a2c29d7efbd3ab3612b03e
shuvo794 <shuvohossain794@gmail.com>
2022-11-16 16:10:12 +0600

    edit

--- leep_year.c
+++ leep_year.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main (void){
-int year=2020;
+int year;
 int is_year=0;
-
+printf("Enter a Year  - ");
+scanf("%d",&year);
 if(((year%4==0 && year%100!=0))||(year%400==0)){
 
 is_year=1;
 
 }
 if(is_year){
-printf("%d is year \n",year);
+printf("%d is leap year \n",year);
 }else{
-    printf("%d is not year \n",year);
+    printf("%d is leap not year \n",year);
 }
 
     return 0;
